318165fe5cf747d6025b4766f8447ef4861d5e89
Rafli ramadhia <rafli.ramadhia@gmail.com>
2019-07-27 02:11:51 +0700

    add LarapixServiceProvider

---
+++ src/Providers/LarapixServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Larapix\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LarapixServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        # code...
+    }
+
+    public function register()
+    {
+        # code...
+    }
+}
